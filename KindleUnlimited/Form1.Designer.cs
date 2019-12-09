472cd1347b0185e9dd43e98979f4905e98364b67
Toshihiko Ichida <dogatana@gmail.com>
2019-12-09 22:19:53 +0900

    initial commit for GitHub

---
+++ KindleUnlimited/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace KindleUnlimited
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonCopyToClipboard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSearch.Location = new System.Drawing.Point(380, 13);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(118, 64);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Browser";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.UseWaitCursor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textSearch
+            // 
+            this.textSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textSearch.Location = new System.Drawing.Point(12, 13);
+            this.textSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(345, 25);
+            this.textSearch.TabIndex = 1;
+            this.textSearch.UseWaitCursor = true;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMessage.Location = new System.Drawing.Point(12, 55);
+            this.textBoxMessage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(345, 133);
+            this.textBoxMessage.TabIndex = 2;
+            this.textBoxMessage.UseWaitCursor = true;
+            // 
+            // buttonCopyToClipboard
+            // 
+            this.buttonCopyToClipboard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCopyToClipboard.Location = new System.Drawing.Point(380, 95);
+            this.buttonCopyToClipboard.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonCopyToClipboard.Name = "buttonCopyToClipboard";
+            this.buttonCopyToClipboard.Size = new System.Drawing.Size(118, 64);
+            this.buttonCopyToClipboard.TabIndex = 3;
+            this.buttonCopyToClipboard.Text = "Clipboard";
+            this.buttonCopyToClipboard.UseVisualStyleBackColor = true;
+            this.buttonCopyToClipboard.UseWaitCursor = true;
+            this.buttonCopyToClipboard.Click += new System.EventHandler(this.buttonCopyToClipboard_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 201);
+            this.Controls.Add(this.buttonCopyToClipboard);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.buttonSearch);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(536, 240);
+            this.Name = "Form1";
+            this.Text = "KindleUnlimited Search";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonCopyToClipboard;
+    }
+}
+
---
+++ KindleUnlimited/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace KindleUnlimited
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            var url = ComposeSearchUrl(textSearch.Text);
+            if (string.IsNullOrEmpty(url))
+                return;
+
+            UpdateMessage(url);
+            System.Diagnostics.Process.Start(url);
+        }
+        private void buttonCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            var url = ComposeSearchUrl(textSearch.Text);
+            if (string.IsNullOrEmpty(url))
+                return;
+
+            UpdateMessage(url);
+            Clipboard.SetText(url);
+        }
+
+        private string ComposeSearchUrl(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return "";
+            const string head = "https://www.amazon.co.jp/s/ref=sr_nr_p_n_feature_nineteen_0?rh=n:2250738051,k:";
+            const string tail = ",p_n_feature_nineteen_browse-bin:3169286051";
+            var encoded_text = System.Web.HttpUtility.UrlEncode(text);
+            return head + encoded_text + tail;
+        }
+
+        private void UpdateMessage(string text)
+        {
+            textBoxMessage.AppendText(text + Environment.NewLine + Environment.NewLine);
+        }
+    }
+
+}
